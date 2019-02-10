a6e594693616515140ff906b704c8f68d71ad216
BrianD147 <briandoyle147@gmail.com>
2019-02-10 01:12:19 +0000

    Adding duplicate letter and invalid input messages

--- app.rb
+++ app.rb
@@ -40,12 +40,12 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    #begin
-    #  flash[:message] = "You have already used that letter." if !@game.guess letter
-    #rescue
-    #  flash[:message] = "Invalid guess."
-    #end
-    @game.guess letter
+    begin
+      flash[:message] = "You have already used that letter." if !@game.guess letter
+    rescue
+      flash[:message] = "Invalid guess."
+    end
+    #@game.guess letter
     redirect '/show'
   end
   
